185918e80124645dd3ad6c322ab3071190600a6b
DESTRYANARC <destryanarahmichairani@gmail.com>
2022-06-05 13:50:46 +0700

    challenge-05

--- index.js
+++ index.js
@@ -6,20 +6,37 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (listnames, hasilSorting) => {
+  const newhasilSorting = hasilSorting(listnames);
+  let no = 0;
+  let newList = [];
+  for(let i = 0; i < newhasilSorting.length; i++){
+    no = i+1;
+    newList.push(`${no}. ${newhasilSorting[i]}`);
+  }
+  return newList;
+};
+
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (listnames) => {
+  return listnames.sort();
+};
+
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (listnames) => {
+  listnames.sort();
+  return listnames.reverse();
+  
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
-  console.log(sorter?.(names, sortAscending)?.join("\n"));
-  console.log(sorter?.(names, sortDescending)?.join("\n"));
+   console.log(sorter?.(names, sortAscending)?.join("\n"));
+   console.log(sorter?.(names, sortDescending)?.join("\n"));
 })();
 
 module.exports = {
